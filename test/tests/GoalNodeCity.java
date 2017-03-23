3beeb850ab0582807ecff130b8d867423e212c58
Sean Gibbs <gibbss@tcd.ie>
2017-03-23 11:15:46 +0000

    Initial Commit

---
+++ test/tests/GoalNodeCity.java
@@ -0,0 +1,25 @@
+package tests;
+
+
+
+import astar.IGoalNode;
+import astar.ISearchNode;
+
+/**
+ * Test case from wikipedia
+ * http://de.wikipedia.org/wiki/A*-Algorithmus
+ */
+
+public class GoalNodeCity implements IGoalNode {
+    private String name;
+    public GoalNodeCity(String name) {
+        this.name = name;
+    }
+    public boolean inGoal(ISearchNode other) {
+         if(other instanceof SearchNodeCity) {
+            SearchNodeCity otherNode = (SearchNodeCity) other;
+            return (this.name == otherNode.getName());
+        }
+        return false;
+    }
+}
